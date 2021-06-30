637c983c7d3d1bd1610465b75d86d79bc9229921
glblduh <gianlloyd.laureano@gmail.com>
2021-06-30 14:27:19 +0800

    Backend done, frontend in progress

---
+++ README.md
@@ -0,0 +1,9 @@
+# WebMonitor
+
+Monitor packets going in and out of a specific interface with a web interface. 
+This needs libpcap-dev to be installed
+
+Instructions are as follows
+1. Clone or download the repo
+2. Run npm install
+3. Start with root privileges
---
+++ index.js
@@ -0,0 +1,69 @@
+/*
+
+	Made by glblduh
+	GitHub: https://github.com/glblduh/WebMonitor
+
+*/
+
+const express = require("express"), http = require("http"), app = express(), server = http.createServer(app), { Server } = require("socket.io"), io = new Server(server);
+let pcap = require("pcap"), pcap_session = pcap.createSession("enx00e04c6801e4");
+let databuf = [];
+
+let buflimit = 20; // The number of packets shown
+let bufconca = true; // If true, concatenates size of duplicates (Slow)
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+
+pcap_session.on("packet", rp => {
+	//Converts raw packets to Object
+	let packet = pcap.decode.packet(rp);
+	let saddr, daddr, sport, dport;
+	//Check if any value is undefined
+	if (packet.payload.payload.saddr != undefined) {
+		saddr = packet.payload.payload.saddr.addr.join(".");
+	} else {
+		saddr = "NULLADDR";
+	}
+	if (packet.payload.payload.daddr != undefined) {
+		daddr = packet.payload.payload.daddr.addr.join(".");
+	} else {
+		daddr = "NULLADDR";
+	}
+	if (packet.payload.payload.payload != undefined && packet.payload.payload.payload.sport != undefined && packet.payload.payload.payload.dport != undefined) {
+		sport = packet.payload.payload.payload.sport;
+		dport = packet.payload.payload.payload.dport;
+	} else {
+		sport = 0;
+		dport = 0;
+	}
+	//Store to buffer until reach the limit
+	if (databuf.length != buflimit) {
+		//Check if addresses are valid
+		if ((saddr.match(/\./g)||[]).length < 4 && (daddr.match(/\./g)||[]).length < 4) {
+			if (!function (){
+				if (bufconca) {
+					//Concatenates size of duplicate packets
+					for(let i=0;i<databuf.length;i++) {
+						if (databuf[i].saddr === saddr && databuf[i].sport === sport && databuf[i].daddr === daddr && databuf[i].dport === dport) {
+							databuf[i].size += packet.pcap_header.len;
+							return true;
+						}
+					}
+				} else {
+					return false;
+				}
+			}()) {
+				//Push source address, source port, destination address, destination port, size of packet to array
+				databuf.push(JSON.parse('{"saddr":"'+saddr+'", "sport":'+sport+', "daddr":"'+daddr+'", "dport":'+dport+', "size":'+packet.pcap_header.len+'}'));
+			}
+		}
+	} else {
+		//Sends array to web
+		io.emit("databuf", databuf);
+		databuf = [];
+	}
+});
+
+server.listen(6699);
