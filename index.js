ab5892000e238c9493b71e9d9a502bc33d370fab
glblduh <gianlloyd.laureano@gmail.com>
2021-07-01 09:16:15 +0800

    Removed bufconca

--- index.js
+++ index.js
@@ -7,7 +7,6 @@
 
 let ifname = "enx00e04c6801e4"; // The name of the interface
 let buflimit = 30; // The number of packets shown
-let bufconca = true; // If true, concatenates size of duplicates (Slow)
 
 const express = require("express"), http = require("http"), app = express(), server = http.createServer(app), { Server } = require("socket.io"), io = new Server(server);
 let pcap = require("pcap"), pcap_session = pcap.createSession(ifname);
@@ -25,7 +24,6 @@ pcap_session.on("packet", rp => {
 		//Check if addresses are valid and if any value is undefined
 		if (packet.payload.payload.saddr != undefined && packet.payload.payload.daddr != undefined && packet.payload.payload.payload != undefined && packet.payload.payload.payload.sport != undefined && packet.payload.payload.payload.dport != undefined && (packet.payload.payload.saddr.addr.join(".").match(/\./g)||[]).length < 4 && (packet.payload.payload.daddr.addr.join(".").match(/\./g)||[]).length < 4) {
 			if (!function (){
-				if (bufconca) {
 					//Concatenates size of duplicate packets
 					for(let i=0;i<databuf.length;i++) {
 						if (databuf[i].saddr === packet.payload.payload.saddr.addr.join(".") && databuf[i].sport === packet.payload.payload.payload.sport && databuf[i].daddr === packet.payload.payload.daddr.addr.join(".") && databuf[i].dport === packet.payload.payload.payload.dport) {
@@ -33,9 +31,6 @@ pcap_session.on("packet", rp => {
 							return true;
 						}
 					}
-				} else {
-					return false;
-				}
 			}()) {
 				//Push source address, source port, destination address, destination port, size of packet to array
 				databuf.push(JSON.parse('{"saddr":"'+packet.payload.payload.saddr.addr.join(".")+'", "sport":'+packet.payload.payload.payload.sport+', "daddr":"'+packet.payload.payload.daddr.addr.join(".")+'", "dport":'+packet.payload.payload.payload.dport+', "proto":'+packet.payload.payload.protocol+', "size":'+packet.pcap_header.len+'}'));
