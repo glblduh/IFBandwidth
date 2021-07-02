097c12955c9bab5482ced78ecff9d611c771afb3
glblduh <gianlloyd.laureano@gmail.com>
2021-07-02 09:38:40 +0800

    Added hostname cache for significantly faster hostname lookup

--- index.js
+++ index.js
@@ -12,8 +12,9 @@
 const express = require("express"), http = require("http"), app = express(), server = http.createServer(app), { Server } = require("socket.io"), io = new Server(server), argv = require("minimist")(process.argv.splice(2)), dns = require("dns").promises;
 if (argv.i == undefined || typeof argv.i === "boolean") {console.error("Please enter a interface name using the -i argument"); process.exit();}
 let pcap = require("pcap"), pcap_session = pcap.createSession(argv.i);
-let databuf = [];
-let sizebuf = 0;
+let databuf = []; // Buffer of packet information
+let sizebuf = 0; // Buffer of the total size of all packets
+let rdnscache = []; // Cache of the hostname of ip from reverse dns
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
@@ -32,10 +33,21 @@ setInterval(() => {
 
 //Handles the reverse dns and handles rejections
 async function reversedns(ip) {
+	//Checks hostname cache array
+	for(let i=0;i<rdnscache.length;i++) {
+		if (rdnscache[i].ip === ip && rdnscache[i].hn != "none") {
+			return rdnscache[i].hn;
+		} else if(rdnscache[i].ip && rdnscache[i].hn == "none") {
+			return ip;
+		}
+	}
+	//Does reverse dns and cache the hostname
 	try {
 		let hn = await dns.reverse(ip);
+		rdnscache.push(JSON.parse('{"ip":"'+ip+'", "hn":"'+hn[0]+'"}'));
 		return hn[0];
 	} catch(e) {
+		rdnscache.push(JSON.parse('{"ip":"'+ip+'", "hn":"none"}'));
 		return ip;
 	}
 }
@@ -44,22 +56,21 @@ pcap_session.on("packet", async rp => {
 	//Converts raw packets to Object
 	let packet = pcap.decode.packet(rp);
 	//Check if addresses are valid and if any value is undefined
-	if (packet.payload.payload.saddr != undefined && packet.payload.payload.daddr != undefined && packet.payload.payload.payload != undefined && packet.payload.payload.payload.sport != undefined && packet.payload.payload.payload.dport != undefined && (packet.payload.payload.saddr.addr.join(".").match(/\./g)||[]).length < 4 && (packet.payload.payload.daddr.addr.join(".").match(/\./g)||[]).length < 4) {
-		if (!function (){
+	if (packet.payload.payload.saddr != undefined && packet.payload.payload.daddr != undefined && packet.payload.payload.payload != undefined && packet.payload.payload.payload != null && packet.payload.payload.payload.sport != undefined && packet.payload.payload.payload.dport != undefined && (packet.payload.payload.saddr.addr.join(".").match(/\./g)||[]).length < 4 && (packet.payload.payload.daddr.addr.join(".").match(/\./g)||[]).length < 4) {
+		if (!function(){
 				//Concatenates size of duplicate packets
 				for(let i=0;i<databuf.length;i++) {
 					if (databuf[i].saddr === packet.payload.payload.saddr.addr.join(".") && databuf[i].sport === packet.payload.payload.payload.sport && databuf[i].daddr === packet.payload.payload.daddr.addr.join(".") && databuf[i].dport === packet.payload.payload.payload.dport) {
 						databuf[i].time = Date.now();
 						databuf[i].size += packet.pcap_header.len;
-						sizebuf += packet.pcap_header.len;
 						return true;
 					}
 				}
 		}()) {
-			sizebuf += packet.pcap_header.len;
 			//Push source address, source port, destination address, destination port, size of packet to array
 			databuf.push(JSON.parse('{"time":'+Date.now()+', "saddr":"'+packet.payload.payload.saddr.addr.join(".")+'", "shost":"'+await reversedns(packet.payload.payload.saddr.addr.join("."))+'", "sport":'+packet.payload.payload.payload.sport+', "daddr":"'+packet.payload.payload.daddr.addr.join(".")+'", "dhost":"'+await reversedns(packet.payload.payload.daddr.addr.join("."))+'", "dport":'+packet.payload.payload.payload.dport+', "proto":'+packet.payload.payload.protocol+', "size":'+packet.pcap_header.len+'}'));
 		}
+		sizebuf += packet.pcap_header.len;
 	}
 });
 
