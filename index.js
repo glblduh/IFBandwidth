6a2c25559450128db588b5e8bcb725b0eed145b4
glblduh <gianlloyd.laureano@gmail.com>
2021-07-02 15:38:32 +0800

    Fixed freezing while on high load by changing the reverse dns resolving

--- index.js
+++ index.js
@@ -14,11 +14,30 @@ if (argv.i == undefined || typeof argv.i === "boolean") {console.error("Please e
 let pcap = require("pcap"), pcap_session = pcap.createSession(argv.i);
 let databuf = []; // Buffer of packet information
 let sizebuf = 0; // Buffer of the total size of all packets
-let rdnscache = []; // Cache of the hostname of ip from reverse dns
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
+
+//Checks the databuf for non-resolved addresses and tries to resolve it
+setInterval(async () => {
+	for(let i=0;i<databuf.length;i++) {
+		if (databuf[i].shost === "") {
+			try {
+				databuf[i].shost = (await dns.reverse(databuf[i].saddr))[0];
+			} catch(e) {
+				databuf[i].shost = databuf[i].saddr;
+			}
+		}
+		if (databuf[i].dhost === "") {
+			try {
+				databuf[i].dhost = (await dns.reverse(databuf[i].daddr))[0];
+			} catch(e) {
+				databuf[i].dhost = databuf[i].daddr;
+			}
+		}
+	}
+}, 100);
 
 //Rechecks databuf array to remove old packets and sends data to web
 setInterval(() => {
@@ -31,28 +50,7 @@ setInterval(() => {
 	io.emit("databuf", {data: databuf, size: sizebuf});
 }, 500);
 
-//Handles the reverse dns and handles rejections
-async function reversedns(ip) {
-	//Checks hostname cache array
-	for(let i=0;i<rdnscache.length;i++) {
-		if (rdnscache[i].ip === ip && rdnscache[i].hn != "none") {
-			return rdnscache[i].hn;
-		} else if(rdnscache[i].ip && rdnscache[i].hn == "none") {
-			return ip;
-		}
-	}
-	//Does reverse dns and cache the hostname
-	try {
-		let hn = await dns.reverse(ip);
-		rdnscache.push(JSON.parse('{"ip":"'+ip+'", "hn":"'+hn[0]+'"}'));
-		return hn[0];
-	} catch(e) {
-		rdnscache.push(JSON.parse('{"ip":"'+ip+'", "hn":"none"}'));
-		return ip;
-	}
-}
-
-pcap_session.on("packet", async rp => {
+pcap_session.on("packet", rp => {
 	//Converts raw packets to Object
 	let packet = pcap.decode.packet(rp);
 	//Check if addresses are valid and if any value is undefined
@@ -68,7 +66,7 @@ pcap_session.on("packet", async rp => {
 				}
 		}()) {
 			//Push source address, source port, destination address, destination port, size of packet to array
-			databuf.push(JSON.parse('{"time":'+Date.now()+', "saddr":"'+packet.payload.payload.saddr.addr.join(".")+'", "shost":"'+await reversedns(packet.payload.payload.saddr.addr.join("."))+'", "sport":'+packet.payload.payload.payload.sport+', "daddr":"'+packet.payload.payload.daddr.addr.join(".")+'", "dhost":"'+await reversedns(packet.payload.payload.daddr.addr.join("."))+'", "dport":'+packet.payload.payload.payload.dport+', "proto":'+packet.payload.payload.protocol+', "size":'+packet.pcap_header.len+'}'));
+			databuf.push(JSON.parse('{"time":'+Date.now()+', "saddr":"'+packet.payload.payload.saddr.addr.join(".")+'", "shost":"", "sport":'+packet.payload.payload.payload.sport+', "daddr":"'+packet.payload.payload.daddr.addr.join(".")+'", "dhost":"", "dport":'+packet.payload.payload.payload.dport+', "proto":'+packet.payload.payload.protocol+', "size":'+packet.pcap_header.len+'}'));
 		}
 		sizebuf += packet.pcap_header.len;
 	}
